8866282b9c168e7e99d3398f53f9b54cadac55c7
DevGaykar <151054333+DevGaykar@users.noreply.github.com>
2024-08-21 19:49:26 +0000

    basic-design of profile-page

--- Social/Social/settings.py
+++ Social/Social/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/"static",
     os.path.join(BASE_DIR, 'post/static'),
+    os.path.join(BASE_DIR, 'userauths/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
